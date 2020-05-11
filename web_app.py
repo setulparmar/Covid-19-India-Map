b236ea78baaf502ce4e423ba78932843dc86b2ad
setulparmar <63709859+setulparmar@users.noreply.github.com>
2020-05-11 12:39:00 +0530

    Update web_app.py
    update3

--- web_app.py
+++ web_app.py
@@ -6,7 +6,7 @@ import folium
 
 app=dash.Dash(__name__)
 server=app.server
-app.layout=html.Div([html.H1('Covid19-India-Map(By Setul Parmar)'),html.H4('source:https://www.mohfw.gov.in/'),html.Iframe(id='map',srcDoc=open('Covid19India.html','r').read(),width='100%',height='600')])
+app.layout=html.Div([html.H1('Covid19-India-Map(By Setul Parmar)'),html.H3('These numbers are only shown state wise and not district wise.'),html.H3('The First and Second number denotes Total cases and Recovered cases of respective states.'),html.H4('Source:https://www.mohfw.gov.in/'),html.Iframe(id='map',srcDoc=open('Covid19India.html','r').read(),width='100%',height='580'),html.H4('Updated on:10 May 2020')])
 
 app.scripts.config.serve_locally = True
 app.css.config.serve_locally = True
