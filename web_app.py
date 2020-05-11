168b48b2912cfa5849f3530bdb67fbcdf1e25572
setulparmar <63709859+setulparmar@users.noreply.github.com>
2020-05-11 13:13:45 +0530

    Update web_app.py
    update5

--- web_app.py
+++ web_app.py
@@ -6,7 +6,13 @@ import folium
 
 app=dash.Dash(__name__)
 server=app.server
-app.layout=html.Div([html.H1('Covid19-India-Map(By Setul Parmar)',style={'color':'#FBABAB'}),html.H5('These numbers are only shown state wise and not district wise.'),html.H5('The First and Second number denotes Total cases and Recovered cases of respective states.'),html.H6('Source:https://www.mohfw.gov.in/'),html.Iframe(id='map',srcDoc=open('Covid19India.html','r').read(),width='100%',height='580'),html.H4('Updated on:10 May 2020')])
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+app.layout=html.Div(style={'backgroundColor': colors['background']},[html.H1('Covid19-India-Map(By Setul Parmar)',style={'color':'#FBABAB'}),html.H5('These numbers are only shown state wise and not district wise.',style={'color':colors['text']}),html.H5('The First and Second number denotes Total cases and Recovered cases of respective states.',style={'color':colors['text']}),html.H6('Source:https://www.mohfw.gov.in/',style={'color':colors['text']}),html.Iframe(id='map',srcDoc=open('Covid19India.html','r').read(),width='100%',height='580'),html.H4('Updated on:10 May 2020',style={'color':colors['text']})])
 
 app.scripts.config.serve_locally = True
 app.css.config.serve_locally = True
