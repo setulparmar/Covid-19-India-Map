60aaca8fe87bd6507d64ec7de11a8ce1643a0357
Setul Parmar <63709859+setulparmar@users.noreply.github.com>
2020-08-16 20:54:36 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -7,8 +7,17 @@ import folium
 app=dash.Dash(__name__)
 server=app.server
 
+link = 'https://github.com/setulparmar/Covid-19-India-Map'
 
-app.layout=html.Div(style={'backgroundColor':'#111111'},children=[html.H1('Covid19-India-Map(By Setul Parmar)',style={'color':'#FBABAB'}),html.H5('These numbers are only shown state wise and not district wise.',style={'color':'#7FDBFF'}),html.H5('The First and Second number denotes Total cases and Recovered cases of respective states.',style={'color':'#7FDBFF'}),html.H6('Source:https://www.mohfw.gov.in/',style={'color':'#7FDBFF'}),html.Iframe(id='map',srcDoc=open('Covid19India.html','r').read(),width='100%',height='580'),html.H4('Updated on:10 May 2020',style={'color':'#7FDBFF'})])
+app.layout=html.Div(style={'backgroundColor':'#111111'},children=[html.H1('Covid19-India-Map(By- Setul Parmar)',
+                                                                          style={'color':'#FBABAB'}),
+                                                                  html.H5('These numbers are only shown state wise and not district wise.',
+                                                                          style={'color':'#7FDBFF'}),
+                                                                  html.H5('The First and Second number denotes Total cases and Recovered cases of respective states.',
+                                                                          style={'color':'#7FDBFF'}),html.H6('Source:https://www.mohfw.gov.in/',style={'color':'#7FDBFF'}),
+                                                                  html.A(n_clicks='1',href = link),
+                                                                  html.Iframe(id='map',srcDoc=open('Covid19India.html','r').read(),width='100%',height='580'),
+                                                                  html.H4('Dataset updated on:10 May 2020',style={'color':'#7FDBFF'})])
 
 app.scripts.config.serve_locally = True
 app.css.config.serve_locally = True
